12af614d686644fa33d4a8b1920eb5172bb59279
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-12-07 20:01:11 +0530

    Fixed linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import _ from 'lodash';
 import './style.css';
 import refresh from './Images/refresh.png';
 import enter from './Images/enter.png';
 import bar from './Images/bar.png';
 
-let tasks = [{ desc: "Buy fruits", completed: false, index: 0}, { desc: "Reply the pending emails", completed: false, index: 1}, { desc: "Do household work", completed: false, index: 2},]
+const tasks = [{ desc: 'Buy fruits', completed: false, index: 0 }, { desc: 'Reply the pending emails', completed: false, index: 1 }, { desc: 'Do household work', completed: false, index: 2 }];
 
 const todoList = document.querySelector('.todo-list');
 
-tasks.forEach(element => {
+tasks.forEach((element) => {
   todoList.innerHTML += `
     <div class="list-items" id=${element.index}>
       <div>
@@ -18,11 +17,10 @@ tasks.forEach(element => {
       <img alt="bar" src=${bar} class="bar">
     </div>
     <hr>
-  `
+  `;
 });
 
 const imgRefresh = document.getElementById('refresh');
 imgRefresh.src = refresh;
 const imgEnter = document.getElementById('enter');
 imgEnter.src = enter;
-
