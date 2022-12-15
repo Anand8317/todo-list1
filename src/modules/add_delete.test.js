27e1d0cbe0d68d2eac95b1473cbc2227a5adac79
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-12-15 15:06:45 +0530

    Added unit test for deletion when array is empty and fixed linters

--- src/modules/add_delete.test.js
+++ src/modules/add_delete.test.js
@@ -1,19 +1,48 @@
 import newTask from './newTask';
 import tasks from './array';
-import del from './del'
+import del from './del';
 
 describe('Test add and remove functions', () => {
   test('Adding an Item to the array', () => {
-    newTask('First task');
-    newTask('Second task');
-    newTask('Third task');
-    expect(tasks[0].desc).toBe(('First task'));
-    expect(tasks[1].desc).toBe(('Second task'));
-    expect(tasks.length).toEqual(3)
+    // Arrange
+    const newTask1 = 'First task';
+    const newTask2 = 'Second task';
+    const newTask3 = 'Third task';
+    const expectedLength = 3;
+
+    // Act
+    newTask(newTask1);
+    newTask(newTask2);
+    newTask(newTask3);
+
+    // Assert
+    expect(tasks[0].desc).toBe(newTask1);
+    expect(tasks[1].desc).toBe(newTask2);
+    expect(tasks.length).toEqual(expectedLength);
   });
+
   test('Removing an item from the array', () => {
+    // Arrange
+    const newTask2 = 'Second task';
+    const expectedLength = 2;
+
+    // Act
     del(1);
-    expect(tasks).toHaveLength(2);
-    expect(tasks[0].desc).toBe('Second task')
+
+    // Assert
+    expect(tasks).toHaveLength(expectedLength);
+    expect(tasks[0].desc).toBe(newTask2);
+  });
+
+  test('Removing an item from the empty array', () => {
+    // Arrange
+    const expectedLength = 0;
+
+    // Act
+    tasks.length = 0;
+    del(1);
+
+    // Assert
+    expect(tasks).toHaveLength(expectedLength);
   });
 });
